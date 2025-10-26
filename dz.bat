abc7b86c968560c7992da42ab61c09e457155518
xx689 <oeddeibber@gmail.com>
2025-10-26 22:23:58 +0100

    Delete dz.bat

--- dz.bat
+++
@@ -1,8 +0,0 @@
-::                                                            by CHEAT OUSSAMA
-
-netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_nxt\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_msi5\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_msi2\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="C:\Program Files (x86)\SmartGaGa\ProjectTitan\Engine\ProjectTitan.exe"
-::                                                            by CHEAT OUSSAMA
